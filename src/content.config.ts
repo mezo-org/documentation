21a860cc85e86197818bc7c04a84bd835562a0b4
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2025-07-29 07:33:36 -0400

    Revert "Revert "Implement Gitbook to Starlight processing system""

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,11 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { topicSchema } from 'starlight-sidebar-topics/schema'
+import type {SchemaContext} from 'astro:content';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema({ extend: topicSchema }) }),
+	docs: defineCollection({ loader: docsLoader(), schema: (context: SchemaContext) => {
+		return docsSchema()(context).extend({ title: z.string().optional() }).and(topicSchema)
+	} })
 };
